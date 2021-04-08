e41893354fed1f5d73c4b819864e93b9dcd9d3e0
stroupbslayen <29642143+stroupbslayen@users.noreply.github.com>
2021-04-08 15:33:16 -0700

    Delete generator.py

--- generator.py
+++
@@ -1,9 +0,0 @@
-command = """
-@commands.command()
-async def command{version}(self, ctx):
-    print('command {version}')
-"""
-
-for cmd in range(30):
-    cmd = "{:02d}".format(cmd)
-    print(command.format(version=cmd))
