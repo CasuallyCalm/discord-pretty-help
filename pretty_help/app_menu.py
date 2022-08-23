ee220ba4d873468e9ac92c2cff2de774f0503d65
StroupBSlayen <29642143+stroupbslayen@users.noreply.github.com>
2022-08-22 17:29:31 -0700

    Added buttons and navigation

--- pretty_help/app_menu.py
+++ pretty_help/app_menu.py
@@ -1,10 +1,40 @@
-from typing import List
+from turtle import color
+from typing import List, Optional
 
 import discord
 from discord.ext import commands
 from discord.ui import Button, Select, View
 
 from .abc_menu import PrettyMenu
+
+
+class HelpNav(View):
+
+    index = 0
+
+    def __init__(self, pages: List[discord.Embed], timeout: Optional[float] = 180):
+        super().__init__(timeout=timeout)
+        self.page_count = len(pages)
+        self.pages = pages
+
+    @discord.ui.button(label="Previous", style=discord.ButtonStyle.success)
+    async def previous(self, interaction: discord.Interaction, button: Button):
+        self.index -= 1
+        await self.update(interaction)
+
+    @discord.ui.button(label="Next", style=discord.ButtonStyle.primary)
+    async def next(self, interaction: discord.Interaction, button: Button):
+        self.index += 1
+        await self.update(interaction)
+
+    @discord.ui.button(label="Delete", style=discord.ButtonStyle.danger)
+    async def _delete(self, interaction: discord.Interaction, button: Button):
+        await interaction.message.delete()
+
+    async def update(self, interaction: discord.Interaction):
+        await interaction.response.edit_message(
+            embed=self.pages[self.index % self.page_count]
+        )
 
 
 class AppMenu(PrettyMenu):
@@ -14,4 +44,4 @@ class AppMenu(PrettyMenu):
         destination: discord.abc.Messageable,
         pages: List[discord.Embed],
     ):
-        ...
+        await ctx.send(embed=pages[0], view=HelpNav(pages))
